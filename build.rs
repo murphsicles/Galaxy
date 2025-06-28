e1fb8282ac78db1270ec8c6b229cb81d31efe17b
Roy Murphy <roycoins@gmail.com>
2025-06-28 09:45:16 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -6,5 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("protos/consensus.proto")?;
     tonic_build::compile_protos("protos/api.proto")?;
     tonic_build::compile_protos("protos/mining.proto")?;
+    tonic_build::compile_protos("protos/validation.proto")?;
+    tonic_build::compile_protos("protos/overlay.proto")?;
     Ok(())
 }
