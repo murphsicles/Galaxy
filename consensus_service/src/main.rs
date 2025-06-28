0e100cbf1ff34cf53f2e7aad74fc70f03e0f01a6
Roy Murphy <roycoins@gmail.com>
2025-06-28 08:00:59 +0100

    Update main.rs

--- consensus_service/src/main.rs
+++ consensus_service/src/main.rs
@@ -1,3 +1,6 @@
+// TODO: Include generated gRPC code
+// use crate::network:: consensus_service_server::ConsensusServer;
+// use crate::network::PingRequest, PingResponse;
 fn main() {
     println!("Hello from consensus_service!");
 }
