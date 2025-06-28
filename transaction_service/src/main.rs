a2afd595d7d64e26f7e64001726ed62a5326d302
Roy Murphy <roycoins@gmail.com>
2025-06-28 08:04:42 +0100

    Update main.rs

--- transaction_service/src/main.rs
+++ transaction_service/src/main.rs
@@ -1,3 +1,6 @@
+// TODO: Include generated gRPC code
+// use crate::network::transaction_service_server::TransactionServer;
+// use crate::network::PingRequest, PingResponse;
 fn main() {
     println!("Hello from transaction_service!");
 }
