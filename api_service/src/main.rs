22b83d4a05fcf704a66c4bfb5cb28a4728b3c534
Roy Murphy <roycoins@gmail.com>
2025-06-28 07:59:01 +0100

    Update main.rs

--- api_service/src/main.rs
+++ api_service/src/main.rs
@@ -1,3 +1,6 @@
+// TODO: Include generated gRPC code
+// use crate::network::api_service_server::APIServer;
+// use crate::network::PingRequest, PingResponse;
 fn main() {
     println!("Hello from api_service!");
 }
