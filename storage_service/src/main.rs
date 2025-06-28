374e31185d64e8700c28dba6727c9fb2ba4566bf
Roy Murphy <roycoins@gmail.com>
2025-06-28 08:03:48 +0100

    Update main.rs

--- storage_service/src/main.rs
+++ storage_service/src/main.rs
@@ -1,3 +1,6 @@
+// TODO: Include generated gRPC code
+// use crate::network::storag_service_server::StorageServer;
+// use crate::network::PingRequest, PingResponse;
 fn main() {
     println!("Hello from storage_service!");
 }
