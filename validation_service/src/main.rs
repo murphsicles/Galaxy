82ac455c012250cb22e1e5f1badbfe8bf3b51dbc
Roy Murphy <roycoins@gmail.com>
2025-06-28 09:58:00 +0100

    Update main.rs

--- validation_service/src/main.rs
+++ validation_service/src/main.rs
@@ -1,3 +1,7 @@
+// TODO: Include generated gRPC code
+// use crate::validation::validation_service_server::ValidationServer;
+// use crate::validation::MPVRequest, MPVResponse, VerifyTxRequest, VerifyTxResponse;
+
 fn main() {
-    println!("Overlay service running");
+    println!("Validation service running");
 }
