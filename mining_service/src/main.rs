81594ed1afc30034910688391205373a605f19da
Roy Murphy <roycoins@gmail.com>
2025-06-28 08:02:05 +0100

    Update main.rs

--- mining_service/src/main.rs
+++ mining_service/src/main.rs
@@ -1,3 +1,6 @@
+// TODO: Include generated gRPC code
+// use crate::network::mining_service_server::MiningServer;
+// use crate::network::PingRequest, PingResponse;
 fn main() {
     println!("Hello from mining_service!");
 }
