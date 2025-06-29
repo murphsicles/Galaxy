cfccdc6a864018e5b1e481b855c9219c590ba3d3
Roy Murphy <roycoins@gmail.com>
2025-06-29 04:41:31 +0100

    Update main.rs

--- network_service/src/main.rs
+++ network_service/src/main.rs
@@ -1,10 +1,34 @@
+use tonic::{transport::Server, Request, Response, Status};
+use network::network_server::{Network, NetworkServer};
+use network::{PingRequest, PingResponse};
+
 tonic::include_proto!("network");
 
-// TODO: Use the generated gRPC code
-// use network::NetworkServer;
-// use network::PingRequest;
-// use network::PingResponse;
+#[derive(Debug, Default)]
+struct NetworkServiceImpl;
 
-fn main() {
-    println!("Hello from network_service!");
+#[tonic::async_trait]
+impl Network for NetworkServiceImpl {
+    async fn ping(&self, request: Request<PingRequest>) -> Result<Response<PingResponse>, Status> {
+        let req = request.into_inner();
+        let reply = PingResponse {
+            reply: format!("Pong: {}", req.message),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let network_service = NetworkServiceImpl::default();
+
+    println!("Network service listening on {}", addr);
+
+    Server::builder()
+        .add_service(NetworkServer::new(network_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
