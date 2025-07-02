18a0b5946354a9df8020ff4b30b218e4676eded5
Roy Murphy <roycoins@gmail.com>
2025-07-02 02:01:54 +0100

    Add alert_service build.rs for proto compilation

---
+++ alert_service/build.rs
@@ -0,0 +1,10 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir("src")
+        .compile(
+            &["../../protos/alert.proto", "../../protos/auth.proto", "../../protos/metrics.proto"],
+            &["../../protos"],
+        )?;
+    Ok(())
+}
