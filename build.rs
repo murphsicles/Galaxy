26419c6e02c1f9daf05ee28d2836428f25992d6d
Roy Murphy <roycoins@gmail.com>
2025-07-02 02:07:42 +0100

    Update root build.rs to include alert.proto and index.proto

--- build.rs
+++ build.rs
@@ -1,12 +1,23 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("protos/network.proto")?;
-    tonic_build::compile_protos("protos/transaction.proto")?;
-    tonic_build::compile_protos("protos/block.proto")?;
-    tonic_build::compile_protos("protos/storage.proto")?;
-    tonic_build::compile_protos("protos/consensus.proto")?;
-    tonic_build::compile_protos("protos/api.proto")?;
-    tonic_build::compile_protos("protos/mining.proto")?;
-    tonic_build::compile_protos("protos/validation.proto")?;
-    tonic_build::compile_protos("protos/overlay.proto")?;
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir("src")
+        .compile(
+            &[
+                "protos/network.proto",
+                "protos/transaction.proto",
+                "protos/block.proto",
+                "protos/storage.proto",
+                "protos/consensus.proto",
+                "protos/overlay.proto",
+                "protos/validation.proto",
+                "protos/mining.proto",
+                "protos/auth.proto",
+                "protos/alert.proto",
+                "protos/index.proto",
+                "protos/metrics.proto",
+            ],
+            &["protos"],
+        )?;
     Ok(())
 }
