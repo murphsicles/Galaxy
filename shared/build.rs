543e34cd79815fa3bfb7e4022214b028b0122093
Roy Murphy <roycoins@gmail.com>
2025-07-03 07:14:45 +0100

    Update shared/build.rs to use tonic_build::compile_protos

--- shared/build.rs
+++ shared/build.rs
@@ -1,15 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile(
-            &[
-                "../protos/block.proto",
-                "../protos/consensus.proto",
-                "../protos/auth.proto",
-                "../protos/alert.proto",
-                "../protos/metrics.proto",
-            ],
-            &["../protos"],
-        )?;
+    tonic_build::compile_protos(
+        tonic_build::configure()
+            .build_server(false),
+        &[
+            "../protos/block.proto",
+            "../protos/consensus.proto",
+            "../protos/auth.proto",
+            "../protos/alert.proto",
+            "../protos/metrics.proto",
+        ],
+        &["../protos"],
+    )?;
     Ok(())
 }
