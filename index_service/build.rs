5a91fdebe90d5ad8dc144927f5d2e2e7ef64e3d2
Roy Murphy <roycoins@gmail.com>
2025-07-04 06:10:35 +0100

    Fix index_service/build.rs to use tonic_build::compile_protos

--- index_service/build.rs
+++ index_service/build.rs
@@ -1,10 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .out_dir("src")
-        .compile(
-            &["../../protos/index.proto", "../../protos/auth.proto", "../../protos/metrics.proto"],
-            &["../../protos"],
-        )?;
+        .compile_protos(&["../protos/index.proto", "../protos/metrics.proto"], &["../protos"])?;
     Ok(())
 }
