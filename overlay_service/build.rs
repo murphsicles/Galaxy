b2a8fa8c19b2c5f1c27f9dceba1c14ca021ece11
Roy Murphy <roycoins@gmail.com>
2025-07-04 09:41:48 +0100

    rustfmt reformat

--- overlay_service/build.rs
+++ overlay_service/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .compile_protos(&["../protos/overlay.proto", "../protos/metrics.proto"], &["../protos"])?;
+    tonic_build::configure().build_server(true).compile_protos(
+        &["../protos/overlay.proto", "../protos/metrics.proto"],
+        &["../protos"],
+    )?;
     Ok(())
 }
