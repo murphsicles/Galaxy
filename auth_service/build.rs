153f69ea794b6dad269887261c77e9c8635103d0
Roy Murphy <roycoins@gmail.com>
2025-07-04 09:35:31 +0100

    rustfmt reformat

--- auth_service/build.rs
+++ auth_service/build.rs
@@ -1,9 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .compile(
-            &["protos/auth.proto", "protos/metrics.proto"],
-            &["protos"],
-        )?;
+    tonic_build::configure().build_server(true).compile_protos(
+        &["../protos/alert.proto", "../protos/metrics.proto"],
+        &["../protos"],
+    )?;
     Ok(())
 }
