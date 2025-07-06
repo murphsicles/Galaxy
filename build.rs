9c3ac87360e1919c55eecdca040435d9867668bc
Roy Murphy <roycoins@gmail.com>
2025-07-06 05:03:42 +0100

    Redtire build.rs

--- build.rs
+++ build.rs
@@ -1,25 +1,30 @@
 // build.rs
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Temporarily disable proto compilation to debug other build issues
-    // tonic_build::configure()
-    //     .build_server(true)
-    //     .out_dir("src")
-    //     .compile_protos(
-    //         &[
-    //             "protos/network.proto",
-    //             "protos/transaction.proto",
-    //             "protos/block.proto",
-    //             "protos/storage.proto",
-    //             "protos/consensus.proto",
-    //             "protos/overlay.proto",
-    //             "protos/validation.proto",
-    //             "protos/mining.proto",
-    //             "protos/auth.proto",
-    //             "protos/alert.proto",
-    //             "protos/index.proto",
-    //             "protos/metrics.proto",
-    //         ],
-    //         &["protos", "protos/google/api", "protos/google/protobuf"],
-    //     )?;
+    // Skip proto compilation in CI, assuming pre-generated files are in src
+    if std::env::var("CI").is_ok() {
+        println!("Skipping proto compilation in CI environment");
+        return Ok(());
+    }
+
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir("src")
+        .compile_protos(
+            &[
+                "protos/network.proto",
+                "protos/transaction.proto",
+                "protos/block.proto",
+                "protos/storage.proto",
+                "protos/consensus.proto",
+                "protos/overlay.proto",
+                "protos/validation.proto",
+                "protos/mining.proto",
+                "protos/auth.proto",
+                "protos/alert.proto",
+                "protos/index.proto",
+                "protos/metrics.proto",
+            ],
+            &["protos", "protos/google/api", "protos/google/protobuf"],
+        )?;
     Ok(())
 }
