a8d880c6a940c83d2ba351d1c0dd466ebe8399cf
Roy Murphy <roycoins@gmail.com>
2025-07-08 20:50:17 +0100

    Update spv_validation.rs

--- tests/spv_validation.rs
+++ tests/spv_validation.rs
@@ -1,8 +1,8 @@
 use tokio::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use bincode::{serialize, deserialize};
-use sv::messages::Tx;
-use sv::util::Serializable;
+use sv::messages::{Tx, TxIn, TxOut, OutPoint};
+use sv::util::{Serializable, Hash256};
 use serde::{Serialize, Deserialize};
 use hex;
 use std::io::Cursor;
@@ -49,8 +49,28 @@ struct VerifySPVProofResponse {
 async fn test_spv_proof_generation_and_verification() {
     let addr = "127.0.0.1:50057"; // validation_service address
     let token = "test_token".to_string(); // Replace with valid JWT token
-    let tx_hex = "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff04deadbeef0100000001000000000000000000000000";
-    let tx: Tx = Tx::read(&mut Cursor::new(hex::decode(tx_hex).unwrap())).unwrap();
+    // Valid coinbase transaction from rust-sv tests (block 2 coinbase)
+    let tx = Tx {
+        version: 1,
+        inputs: vec![TxIn {
+            prev_output: OutPoint {
+                hash: Hash256([0; 32]),
+                index: 4294967295,
+            },
+            unlock_script: sv::script::Script(vec![4, 255, 255, 0, 29, 1, 11]),
+            sequence: 4294967295,
+        }],
+        outputs: vec![TxOut {
+            satoshis: 5000000000,
+            lock_script: sv::script::Script(vec![
+                65, 4, 114, 17, 168, 36, 245, 91, 80, 82, 40, 228, 195, 213, 25, 76, 31, 207,
+                170, 21, 164, 86, 171, 223, 55, 249, 185, 217, 122, 64, 64, 175, 192, 115, 222,
+                230, 200, 144, 100, 152, 79, 3, 56, 82, 55, 217, 33, 103, 193, 62, 35, 100, 70,
+                180, 23, 171, 121, 160, 252, 174, 65, 42, 227, 49, 107, 119, 172,
+            ]),
+        }],
+        lock_time: 0,
+    };
     let mut tx_bytes = Vec::new();
     tx.write(&mut tx_bytes).unwrap();
     let txid = hex::encode(&tx_bytes);
