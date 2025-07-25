210f3dd5afc5e019dc99925da7ea7dee8d3385fb
Roy Murphy <roycoins@gmail.com>
2025-07-25 10:09:15 +0100

    Update torrent_service.rs

--- tests/torrent_service.rs
+++ tests/torrent_service.rs
@@ -3,9 +3,9 @@ use bincode::{deserialize, serialize};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::time::{sleep, Duration};
-use tracing::info;
+use tracing::{info, error};
 use sv::block::{Block, Header};
-use sv::transaction::Transaction as SvTx;
+use sv::transaction::{Transaction as SvTx, OutPoint};
 use sv::merkle::MerklePath;
 use shared::AgedThreshold;
 use torrent_service::service::{TorrentService, GetAgedBlocksRequest, GetAgedBlocksResponse, TorrentRequestType, TorrentResponseType};
@@ -22,6 +22,7 @@ enum MockRequestType {
     BroadcastTx(BroadcastTxRequest),
     AuthRequest(AuthRequest),
     AlertRequest(AlertRequest),
+    GetUtxos(GetUtxosRequest),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -32,6 +33,7 @@ enum MockResponseType {
     BroadcastTx(BroadcastTxResponse),
     AuthResponse(AuthResponse),
     AlertResponse(AlertResponse),
+    GetUtxos(GetUtxosResponse),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -92,6 +94,24 @@ struct AlertRequest {
 struct AlertResponse {
     success: bool,
     error: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetUtxosRequest {
+    address: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetUtxosResponse {
+    utxos: Vec<Utxo>,
+    error: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Utxo {
+    outpoint: OutPoint,
+    amount: u64,
+    script_pubkey: String,
 }
 
 #[tokio::test]
@@ -239,6 +259,37 @@ async fn test_torrent_service_end_to_end() {
                         error: String::new(),
                     };
                     let encoded = serialize(&MockResponseType::AlertResponse(resp)).unwrap();
+                    stream.write_all(&encoded).await.unwrap();
+                    stream.flush().await.unwrap();
+                }
+                _ => {}
+            }
+        }
+    });
+
+    // Mock storage_service for UTXOs
+    let storage_listener = TcpListener::bind("127.0.0.1:50053").await.unwrap();
+    tokio::spawn(async move {
+        loop {
+            let (mut stream, _) = storage_listener.accept().await.unwrap();
+            let mut buffer = vec![0u8; 1024 * 1024];
+            let n = stream.read(&mut buffer).await.unwrap();
+            let req: MockRequestType = deserialize(&buffer[..n]).unwrap();
+            match req {
+                MockRequestType::GetUtxos(_) => {
+                    let utxo = Utxo {
+                        outpoint: OutPoint {
+                            txid: "dummy_txid".to_string(),
+                            vout: 0,
+                        },
+                        amount: 1000000, // 0.01 BSV
+                        script_pubkey: "76a91488a5e4a4e6c4a4e0c7b0b4a4e4a4e4a4e4a4e4a488ac".to_string(),
+                    };
+                    let resp = GetUtxosResponse {
+                        utxos: vec![utxo],
+                        error: String::new(),
+                    };
+                    let encoded = serialize(&MockResponseType::GetUtxos(resp)).unwrap();
                     stream.write_all(&encoded).await.unwrap();
                     stream.flush().await.unwrap();
                 }
